432d5ef6fa7a1fc1a312f48b8d9397309133a403
Jorge urbina <jorgearmandou@gmail.com>
2017-04-05 22:57:49 -0500

    contador

---
+++ app/src/main/java/armando/com/reon/contador/MainActivity.java
@@ -0,0 +1,40 @@
+package armando.com.reon.contador;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+    public int contador;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        contador = 0;
+    }
+
+    public void incrementoContador(View vista){
+        contador++;
+        mostrarResultado();
+    }
+
+    public void decrementoContador(View vista){
+        contador--;
+        mostrarResultado();
+    }
+
+    public void reset(View vista){
+        contador = 0;
+        mostrarResultado();
+    }
+
+    public void mostrarResultado(){
+        TextView textoResultado = (TextView)findViewById(R.id.ContadorTexto);
+        textoResultado.setText("" + contador);
+    }
+}
